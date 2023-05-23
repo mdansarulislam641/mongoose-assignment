9c927e7358aa6e5a38e0a7dd51abd249da5bb44b
mdansarulislam641 <mdansarulislam641.com>
2023-05-23 20:46:41 +0600

    Book model crate done

--- src/app/modules/books/book.interface.ts
+++ src/app/modules/books/book.interface.ts
@@ -1,15 +1,15 @@
 
 // interface for Book collections 
-interface IBook {
-    title : string ,
-    author : string[] ,
-    genre : string ,
-    publicationYear : number ,
-    publisher : {
-        name : string ,
-        location : string 
-    } ,
-    reviews :{user : string , comment : string}[]  ,
-    rating : number ,
-    price : string | number 
+export interface IBook {
+    title: string,
+    author: string[],
+    genre: string,
+    publicationYear: number,
+    publisher: {
+        name: string,
+        location: string
+    },
+    reviews: { user: string, comment: string }[],
+    rating: number,
+    price: string | number
 }
---
+++ src/app/modules/books/book.model.ts
@@ -0,0 +1,54 @@
+import { Schema, model } from "mongoose";
+import { IBook } from "./book.interface";
+
+// publisher publicationYear genre author
+const bookSchema = new Schema<IBook>({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: [String],
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    publicationYear: {
+        type: Number,
+        required: true
+    },
+    publisher: {
+        name: {
+            type: String,
+            required: true
+        },
+        location: {
+            type: String,
+            required: true
+        }
+    },
+    reviews: [
+        {
+            user: {
+                type: String,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }],
+        rating : {
+            type : Number ,
+            required : true 
+        },
+        price : {
+            type : Number || String ,
+            required: true
+        }
+});
+
+const Book = model<IBook>('Books', bookSchema);
+export default Book ; 
