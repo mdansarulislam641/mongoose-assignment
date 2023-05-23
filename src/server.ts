d7683e6054d87e3f84ec53a19ec909e67f9e33b5
mdansarulislam641 <mdansarulislam641.com>
2023-05-23 20:07:17 +0600

    book interface create done

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-// getting-started.js
-const mongoose = require('mongoose');
-
-main().catch(err => console.log(err));
+import mongoose from "mongoose";
 
 async function main() {
 try {
     await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    console.log("Database Connection Successful")
 
 } catch (error) {
     console.log('Database Connection Failed');
 }
 }
+main();
